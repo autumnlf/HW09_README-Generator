a0a9e2b702a5a64737b378bf9d484e4f725621d7
Autumn Fields <autumnF12@gmail.com>
2022-04-15 22:59:44 -0400

    Updated js

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Choices = require('inquirer/lib/objects/choices');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please provide a link to your github profile:',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message:  'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'The link to your github or deployment:', 
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please give a description for your project:', 
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please give installation instructions:', 
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Usage information:', 
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Contribution guidelines:',
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: "Test Instructions:", 
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: "Choose any licensing needed:",
+            choices: ['MIT', 'Mozilla', 'Apache 2.0', 'Boost', 'none'],
+        },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile('exampleREADME.md', data, (err) => {
+        err ? console.log(err) : console.log('Go Check')
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+    .then((data) => writeToFile('exampleREADME.md', generateMarkdown(data)))
+    .then(() => console.log('Successfully wrote to README'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,118 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+ let badge = ''
+  if ( data.license === 'MIT'){
+    badge = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+  };
+  if ( data.license === 'Mozilla'){
+    badge = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)`;
+  };
+  if ( data.license === 'Apache 2.0'){
+    badge = `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+  };
+  if ( data.license === 'Boost'){
+    badge = `![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)`;
+  };
+  if ( data.license === 'none'){
+    badge = ``;
+  };
+  return badge
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  let link = ''
+  if ( data.license === 'MIT'){
+    link = `https://opensource.org/licenses/MIT`;
+  };
+  if ( data.license === 'Mozilla'){
+    link = `https://opensource.org/licenses/MPL-2.0`;
+  };
+  if ( data.license === 'Apache 2.0'){
+    link = `https://opensource.org/licenses/Apache-2.0`;
+  };
+  if ( data.license === 'Boost'){
+    link = `https://www.boost.org/LICENSE_1_0.txt`;
+  };
+  if ( data.license === 'none'){
+    link = ``;
+  };
+  return link
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  let sectionLicense = ''
+  if ( data.license === 'none'){
+    sectionLicense = ``;
+  };
+  if (data.license !== 'none') {
+    sectionLicense = '## License';
+  };
+  return sectionLicense
+}
+
+function renderLicenseTOC(data) {
+  let licenseTOC = ''
+  if ( data.license === 'none'){
+    licenseTOC = ``;
+  };
+  if (data.license !== 'none') {
+    licenseTOC = '* [License](#license)';
+  };
+  return licenseTOC
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  **Link to deployed application:**
+  > ${data.link}
+
+  ## Description
+    ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributions](#contributions)
+  * [Testing](#testing)
+  ${renderLicenseTOC(data)}
+  * [Questions](#questions)
+
+  ##  Installation
+  Instruction for installing and running project:
+
+    ${data.installation}
+
+  ## Usage
+  Examples for use and instruction:
+
+    ${data.usage}
+
+  ## Contributions
+  Instructions for how to make contributions:
+
+    ${data.contribution}
+
+  ## Testing Instructions
+  How to run tests for project:
+
+    ${data.instructions}
+
+  ${renderLicenseSection(data)}
+  ${renderLicenseBadge(data)}
+  
+    ${renderLicenseLink(data)}
+
+  ## Questions
+  For any questions please use the following contact information:
+  * ${data.email}
+  * ${data.github}
 
 `;
 }
